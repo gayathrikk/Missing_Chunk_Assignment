66965bb2208288b4708099596e3886d0541d24d2
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-12-10 10:26:42 +0530

    Update Missing_chunk_Assignment.java

--- src/test/java/com/test/Database_Testing/Missing_chunk_Assignment.java
+++ src/test/java/com/test/Database_Testing/Missing_chunk_Assignment.java
@@ -14,7 +14,7 @@ public class Missing_chunk_Assignment {
 
     private final String host = "pp6.humanbrain.in";
     private final String user = "hbp";
-    private final String sshPassword = "Health#123";
+    private final String sshPassword = "hbpsgbclab@123";
     private final String basePath = "/mnt/remote/tapebackup/staging";
     private final String fileName = "chunk_assignments.json";
 
@@ -167,5 +167,6 @@ public class Missing_chunk_Assignment {
         }
     }
 }
+
 
 
